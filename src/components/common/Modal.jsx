3cc075ec10799bc973903b719126d4c52dd75937
Bartu Özaşçı <bartuozasci@gmail.com>
2026-02-24 14:18:39 +0300

    v1.7 - Modal her zaman ekranın merkezinde açılıyor
    - items-end kaldırıldı, modal artık mobilde de viewport'un ortasında görünüyor

--- src/components/common/Modal.jsx
+++ src/components/common/Modal.jsx
@@ -17,7 +17,7 @@ export default function Modal({ isOpen, onClose, title, children }) {
 
   return (
     <div
-      className="fixed inset-0 z-50 flex items-end sm:items-center justify-center p-4"
+      className="fixed inset-0 z-50 flex items-center justify-center p-4"
       onClick={onClose}
     >
       {/* Backdrop */}
